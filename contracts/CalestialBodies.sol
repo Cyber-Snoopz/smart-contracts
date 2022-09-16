389ea77263999a6f5d086fcd58c0f6741b8f4c14
yg18 <y1ngp_342.speed@dsm.gg>
2022-09-16 14:35:06 +0530

    added a deployment script

--- contracts/CalestialBodies.sol
+++ contracts/CalestialBodies.sol
@@ -12,7 +12,7 @@ contract CalestialBodies is ERC1155, AccessControl, Pausable, ERC1155Supply {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC1155("") {
+    constructor(string memory newuri) ERC1155(newuri) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(URI_SETTER_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
---
+++ scripts/deploy.ts
@@ -0,0 +1,36 @@
+import { ethers, run } from "hardhat";
+
+const tokenURI = "";
+
+async function main() {
+  const CalestialBodies = await ethers.getContractFactory("CalestialBodies");
+  const nftCollection = await CalestialBodies.deploy(tokenURI);
+
+  await nftCollection.deployTransaction.wait(2);
+
+  await run("verify:verify", {
+    address: nftCollection.address,
+    constructorArguments: [tokenURI],
+  });
+
+  const RewardHandler = await ethers.getContractFactory("RewardHandler");
+  const rewardHandler = await RewardHandler.deploy(nftCollection.address);
+
+  await rewardHandler.deployTransaction.wait(2);
+
+  await run("verify:verify", {
+    address: rewardHandler.address,
+    constructorArguments: [nftCollection.address],
+  });
+
+  console.log(
+    `NFT Collection is deployed to ${nftCollection.address} and the Reward Handler is deployed to ${rewardHandler.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
